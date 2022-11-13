665e9cb23f608c06f76f3571a933d4fb483368f8
Autumn Jing Green <67246658+autumngreenbean@users.noreply.github.com>
2022-11-12 19:01:46 -0800

    Dee dee + experimenting with H. scroll

---
+++ testjava.js
@@ -0,0 +1,10 @@
+const element = document.querySelector("#container");
+
+element.addEventListener('wheel', (event) => {
+  event.preventDefault();
+
+  element.scrollBy({
+    left: event.deltaY < 0 ? -30 : 30,
+    
+  });
+});
